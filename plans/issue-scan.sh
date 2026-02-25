226ebdd7f5b18853a4b9419ec9f4a73072be5828
Martin Pitt <martin@piware.de>
2026-02-25 10:04:05 +0100

    fix perm

--- plans/issue-scan.sh
+++ plans/issue-scan.sh
@@ -4,10 +4,10 @@ set -eu
 # Set up secrets BEFORE enabling -x
 mkdir -p secrets/s3-keys
 echo "$GITHUB_TOKEN" > secrets/github-token
-chmod 600 secrets/github-token
+chmod 644 secrets/github-token
 echo "$S3_KEY_EU" > secrets/s3-keys/eu-central-1.linodeobjects.com
 echo "$S3_KEY_US" > secrets/s3-keys/us-east-1.linodeobjects.com
-chmod 600 secrets/s3-keys/*
+chmod 644 secrets/s3-keys/*
 # Parse S3 key (format: "ACCESS SECRET")
 read -r S3_ACCESS S3_SECRET <<< "$S3_KEY_LOGS"
 
@@ -44,11 +44,11 @@ run-args = [
 
 [container.secrets]
 github-token = [
-    '--volume=$PWD/secrets/github-token:/run/secrets/github-token:U,ro',
+    '--volume=$PWD/secrets/github-token:/run/secrets/github-token:ro',
     '--env=COCKPIT_GITHUB_TOKEN_FILE=/run/secrets/github-token',
 ]
 image-upload = [
-    '--volume=$PWD/secrets/s3-keys:/run/secrets/s3-keys:U,ro',
+    '--volume=$PWD/secrets/s3-keys:/run/secrets/s3-keys:ro',
     '--env=COCKPIT_S3_KEY_DIR=/run/secrets/s3-keys',
 ]
 TOML_EOF
